3a31503ae2a365b57ed3790db76eae12ba3e8f62
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-02-13 08:58:39 -0300

    feat: update customer

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -4,7 +4,6 @@ export async function insertCustomer(req, res) {
     const { name, phone, cpf, birthday } = req.body
     try {
         const isCpfNotAvailable = await db.query(`SELECT cpf FROM customers WHERE cpf = $1;`, [cpf])
-        console.log(isCpfNotAvailable.rows)
         if (isCpfNotAvailable.rows.length !== 0) return res.sendStatus(409)
         await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`, [name, phone, cpf, birthday])
         return res.send(201)
@@ -24,10 +23,22 @@ export async function listCustomers(req, res) {
 }
 
 export async function listCustomersById(req, res) {
-    const {id} = req.params
+    const { id } = req.params
     try {
         const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
-        res.status(200).send(customer.rows)
+        if (customer.rows.length === 0) return res.sendStatus(404)
+        return res.status(200).send(customer.rows)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+export async function updateCustomer(req, res) {
+    const { id } = req.params
+    const { name, phone, cpf, birthday } = req.body
+    try {
+        await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`, [name, phone, cpf, birthday, id])
+        return res.send(200)
     } catch (err) {
         return res.status(500).send(err.message)
     }
--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -6,7 +6,7 @@ const Joi = JoiBase.extend(JoiDate)
 
 const customersSchema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
-    phone: Joi.string().regex(/^[0-9]{11}$/).required(),
+    phone: Joi.string().pattern(/^[0-9]+$/, 'numbers').min(10).max(11).required(),
     cpf: Joi.string().regex(/^[0-9]{11}$/).required(),
     birthday: Joi.date().format("YYYY-MM-DD").required()
 })
