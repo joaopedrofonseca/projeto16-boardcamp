20126ae9f610a396492a74f6794cef62c719bff8
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-02-13 08:43:24 -0300

    fix: cpf and phone customer schema

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -6,8 +6,8 @@ const Joi = JoiBase.extend(JoiDate)
 
 const customersSchema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
-    phone: Joi.string().length(11).required(),
-    cpf: Joi.string().length(11).required(),
+    phone: Joi.string().regex(/^[0-9]{11}$/).required(),
+    cpf: Joi.string().regex(/^[0-9]{11}$/).required(),
     birthday: Joi.date().format("YYYY-MM-DD").required()
 })
 
