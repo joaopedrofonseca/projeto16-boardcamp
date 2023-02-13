abf31b488fc81cbc2e405e9646fa238a5cd00ecd
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-02-13 09:06:38 -0300

    fix: update customer checking cpfs

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -37,7 +37,10 @@ export async function updateCustomer(req, res) {
     const { id } = req.params
     const { name, phone, cpf, birthday } = req.body
     try {
-        await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`, [name, phone, cpf, birthday, id])
+        const isCpfNotAvailable = await db.query(`SELECT cpf FROM customers WHERE cpf = $1 AND id!=$2;`, [cpf, id])
+        if (isCpfNotAvailable.rows.length !== 0) return res.sendStatus(409)
+
+        await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5;`, [name, phone, cpf, birthday, id])
         return res.send(200)
     } catch (err) {
         return res.status(500).send(err.message)
