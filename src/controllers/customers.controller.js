aca42ba18d88a5898a6366038bb6caa60f39a90e
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-02-13 08:34:23 -0300

    feat: search customer by id

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,7 +1,7 @@
 import { db } from "../database/database.connection.js"
 
 export async function insertCustomer(req, res) {
-    const {name, phone, cpf, birthday} = req.body
+    const { name, phone, cpf, birthday } = req.body
     try {
         const isCpfNotAvailable = await db.query(`SELECT cpf FROM customers WHERE cpf = $1;`, [cpf])
         console.log(isCpfNotAvailable.rows)
@@ -14,11 +14,21 @@ export async function insertCustomer(req, res) {
     }
 }
 
-export async function listCustomers(req,res){
-    try{
+export async function listCustomers(req, res) {
+    try {
         const customers = await db.query(`SELECT * FROM customers;`)
         return res.status(200).send(customers.rows)
-    }catch(err){
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+export async function listCustomersById(req, res) {
+    const {id} = req.params
+    try {
+        const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
+        res.status(200).send(customer.rows)
+    } catch (err) {
         return res.status(500).send(err.message)
     }
 }
--- src/routes/customers.router.js
+++ src/routes/customers.router.js
@@ -1,13 +1,13 @@
 import { Router } from 'express'
-import { insertCustomer, listCustomers } from '../controllers/customers.controller.js'
+import { insertCustomer, listCustomers, listCustomersById } from '../controllers/customers.controller.js'
 import { validateSchema } from '../middlewares/validateSchema.js'
 import customersSchema from '../schemas/customersSchema.js'
 
 const customersRouter = Router()
 
 customersRouter.get("/customers", listCustomers)
+customersRouter.get("/customers/:id", listCustomersById)
 customersRouter.post("/customers", validateSchema(customersSchema),insertCustomer)
-
 customersRouter.put ("/customers/:id", )
 
 export default customersRouter
